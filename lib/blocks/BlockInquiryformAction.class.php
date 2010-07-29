188e9da359ac9d6a05b7f994c8d98e7848f665dd
Gaël PORT <gael.port@rbs.fr>
2010-07-29 14:40:16 +0200

    [FIX] Handle secured inquiry forms.

--- lib/blocks/BlockInquiryformAction.class.php
+++ lib/blocks/BlockInquiryformAction.class.php
@@ -5,4 +5,20 @@
  */
 class inquiry_BlockInquiryformAction extends form_BlockFormBaseAction
 {
+	/**
+	 * @param inquiry_persistentdocument_inquiryform $form
+	 * @param f_mvc_Request $request
+	 * @return string
+	 */
+	protected function checkAccess($form, $request)
+	{
+		if ($form->getSecured() && users_UserService::getInstance()->getCurrentFrontEndUser() === null)
+		{
+			$agaviUser = Controller::getInstance()->getContext()->getUser();
+			$agaviUser->setAttribute('illegalAccessPage', $_SERVER["REQUEST_URI"]);
+			$this->addError(f_Locale::translate('&modules.inquiry.frontoffice.Error-secured-form;'));
+			return false;
+		}
+		return true;
+	}
 }
